1e186208139db01aba50239227e67e93239e5670
Eclipse <ydomerat@gmail.com>
2023-03-21 15:17:34 +0100

    Initial Commit

---
+++ TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by Yuliia on 21.03.23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
